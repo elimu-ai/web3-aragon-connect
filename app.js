a6f74507f10d54846a2e9a2319abab68cea4483e
Nya Elimu <nya@elimu.ai>
2020-07-12 18:16:24 +0800

    #1 Integrate with Aragon Connect
    - Fix `SyntaxError: Cannot use import statement outside a module`

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 // Node.js webapp for displaying information about the elimu.ai Community DAO at https://mainnet.aragon.org/#elimuai
 
-import { connect } from '@aragon/connect'
+const fetchOrgDataAsync = async() => {
+	console.log('fetchOrgDataAsync');
 
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+	// Fetch data from the DAO
+	const { connect } = require('@aragon/connect');
+	const org = await connect('elimuai.aragonid.eth', 'thegraph');
+	console.log(`org: ${org}`);
 
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World');
-});
+	console.log('\nApps:');
+    const apps = await org.apps();
+    apps.map(console.log);
 
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
+	// Initialize the web server
+	initializeWebServer(apps);
+}
+fetchOrgDataAsync();
+
+function initializeWebServer(apps) {
+	const http = require('http');
+	const hostname = '127.0.0.1';
+	const port = 3000;
+	const server = http.createServer((req, res) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+
+		// Display information about the DAO
+		res.end(`Apps: ${apps}`);
+	});
+	server.listen(port, hostname, () => {
+		console.log(`Server running at http://${hostname}:${port}/`);
+	});
+}
