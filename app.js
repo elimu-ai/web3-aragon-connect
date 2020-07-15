dc0dc035702e3b85893b4c6a040429d0a31f9595
Nya Elimu <nya@elimu.ai>
2020-07-15 19:40:36 +0800

    #5 List votes created and cast by the community's contributors
    - yarn add @aragon/connect-thegraph-voting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const fetchApps = require('./lib/fetch_apps')
 const fetchTokenHolders = require('./lib/fetch_token_holders')
+const fetchVotes = require('./lib/fetch_votes')
 const http = require('http')
 
 const server = express()
@@ -44,6 +45,29 @@ server.get('/token-holders', async (req, res) => {
   tokenHolders.forEach((tokenHolder, i) => {
     console.debug(`tokenHolder.address: "${tokenHolder.address}"`)
     jsonArray.push(tokenHolder)
+  })
+  console.debug(`jsonArray.length: ${jsonArray.length}`)
+  let jsonString = JSON.stringify(jsonArray)
+  res.write(jsonString)
+
+  res.send()
+})
+
+// Fetch the DAO's votes
+server.get('/votes', async (req, res) => {
+  console.info(`req.url: "${req.url}"`)
+
+  res.setHeader('Content-Type', 'application/json')
+
+  const votes = await fetchVotes()
+  votes.map(console.info)
+  console.debug(`votes.length: ${votes.length}`)
+
+  // Convert to JSON
+  let jsonArray = []
+  votes.forEach((vote, i) => {
+    console.debug(`vote.id: "${vote.id}"`)
+    jsonArray.push(vote)
   })
   console.debug(`jsonArray.length: ${jsonArray.length}`)
   let jsonString = JSON.stringify(jsonArray)
