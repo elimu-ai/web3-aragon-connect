3a001e60bd8f6ff1a3cc897b370858e4543450d5
Nya Elimu <nya@elimu.ai>
2020-07-15 16:52:15 +0800

    #11 Add usage of the Express framework
    - Return Apps as JSON array

--- app.js
+++ app.js
@@ -7,8 +7,25 @@ const port = 3000
 
 // Display information about the DAO
 app.get('/apps', async (req, res) => {
+  console.info(`req.url: "${req.url}"`)
+
+  res.setHeader('Content-Type', 'application/json')
+
+  // Fetch the DAO's Apps
   const apps = await fetchApps()
-  res.write(`apps: ${apps}`)
+  apps.map(console.info)
+  console.debug(`apps.length: ${apps.length}`)
+
+  // Convert to JSON
+  let jsonArray = []
+  apps.forEach((item, i) => {
+    console.debug(`item.name: "${item.name}"`)
+    jsonArray.push(item)
+  })
+  console.debug(`jsonArray.length: ${jsonArray.length}`)
+  let jsonString = JSON.stringify(jsonArray)
+  res.write(jsonString)
+
   res.send()
 })
 
