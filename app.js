14be51ed321cc609975566431eaf567046024e08
Nya Elimu <nya@elimu.ai>
2020-07-15 15:12:16 +0800

    #11 Add usage of the Express framework
    - yarn add express

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const fetchApps = require('./lib/fetch_apps')
+const http = require('http')
+
+const app = express()
+const port = 3000
+
+// Display information about the DAO
+app.get('/', async (req, res) => {
+  const apps = await fetchApps()
+  res.write(`apps: ${apps}`)
+  res.send()
+})
+
+app.listen(port, () => {
+  console.info(`App listening at http://localhost:${port}`)
+})
